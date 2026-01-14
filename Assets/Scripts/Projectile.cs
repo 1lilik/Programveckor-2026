d96aaa1e592a34976b1a873af3898602e8370e8b
HugoSkola <151546996+HugoSkola@users.noreply.github.com>
2026-01-14 09:23:25 +0100

    fail

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,13 +6,24 @@ public class Projectile : MonoBehaviour
     EnemyBase enemybaseScript;
     PlayerStats playerStatsScript;
 
+
+    private void Awake()
+    {
+        playerStatsScript = GetComponent<PlayerStats>();
+        enemybaseScript = GetComponent<EnemyBase>();
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.CompareTag("Trigger"))
+        if (!other.CompareTag("Player"))
         {
             Destroy(gameObject);
 
             playerStatsScript.TakeDamage(enemybaseScript.damage);
+        }
+
+        if (!other.CompareTag("Trigger"))
+        {
+            Destroy(gameObject);
         }
     }
 
