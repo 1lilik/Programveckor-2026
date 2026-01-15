1209940350e827eb4e9c726eb417660392abf0e3
Eddpoxy <edward.lundh@elev.ga.lbs.se>
2026-01-15 21:49:36 +0100

    Enemy sprites and animations

--- Assets/Scripts/EnemyLongRange.cs
+++ Assets/Scripts/EnemyLongRange.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemyLongRange : EnemyBase
@@ -5,12 +6,14 @@ public class EnemyLongRange : EnemyBase
     [Header("ProjectileSettings")]
     [SerializeField] private GameObject projectilePrefab;
     [SerializeField] private float projectileSpeed;
+    [SerializeField] private Animator animator;
 
     public override void Attack()
     {
         timer += Time.deltaTime;
         if (timer >= attackTimer)
         {
+            animator.SetTrigger("Shoot");
             GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
             Vector3 direction = (player.transform.position - transform.position).normalized;
 
@@ -23,5 +26,6 @@ public class EnemyLongRange : EnemyBase
             timer = 0;
         }
  
-    }
+    } 
+
 }
