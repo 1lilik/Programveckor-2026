a613438602c694ed17b815779f194cddfc10c782
SebastianEklund1 <sebastian.eklund2@elev.ga.lbs.se>
2026-01-16 13:30:30 +0100

    Menu (graphics)

--- Assets/menuManager.cs
+++ Assets/menuManager.cs
@@ -6,13 +6,12 @@ public class menuManager : MonoBehaviour
     // --Menu panels--
     public GameObject mainMenu;
     public GameObject settingsMenu;
-    public GameObject controlsMenu;
     public GameObject pauseMenu;
     public GameObject audioSettingsMenu;
     public GameObject controlSettingsMenu;
-    public GameObject languageMenu;
     public GameObject gameOverMenu;
     public AudioClip buttonClick;
+
 
     public void Start() //Shows the main menu on start
     {
@@ -22,78 +21,44 @@ public class menuManager : MonoBehaviour
     public void ShowMainMenu() //Shows the main menu
     {
         mainMenu.SetActive(true);
-        controlsMenu.SetActive(false);
         settingsMenu.SetActive(false);
         pauseMenu.SetActive(false);
         audioSettingsMenu.SetActive(false);
         controlSettingsMenu.SetActive(false);
-        languageMenu.SetActive(false);
     }
-
-    public void ShowControlsMenu() //Show the controls menu
-    {
-        mainMenu.SetActive(false);
-        controlsMenu.SetActive(true);
-        settingsMenu.SetActive(false);
-        pauseMenu.SetActive(false);
-        audioSettingsMenu.SetActive(false);
-        controlSettingsMenu.SetActive(false);
-        languageMenu.SetActive(false);
-    }
-
     public void ShowSettingsMenu() //Show the settings menu
     {
         mainMenu.SetActive(false);
-        controlsMenu.SetActive(false);
         settingsMenu.SetActive(true);
         pauseMenu.SetActive(false);
         audioSettingsMenu.SetActive(false);
         controlSettingsMenu.SetActive(false);
-        languageMenu.SetActive(false);
     }
 
     public void ShowAudioSettingsMenu() //Shows the audio settings menu
     {
         mainMenu.SetActive(false);
-        controlsMenu.SetActive(false);
         settingsMenu.SetActive(false);
         pauseMenu.SetActive(false);
         audioSettingsMenu.SetActive(true);
         controlSettingsMenu.SetActive(false);
-        languageMenu.SetActive(false);
     }
 
     public void ShowControlSettingsMenu() //Shows the control settings menu
     {
         mainMenu.SetActive(false);
-        controlsMenu.SetActive(false);
         settingsMenu.SetActive(false);
         pauseMenu.SetActive(false);
         audioSettingsMenu.SetActive(false);
         controlSettingsMenu.SetActive(true);
-        languageMenu.SetActive(false);
     }
-
-    public void ShowLanguageMenu() //Shows the language menu
-    {
-        mainMenu.SetActive(false);
-        controlsMenu.SetActive(false);
-        settingsMenu.SetActive(false);
-        pauseMenu.SetActive(false);
-        audioSettingsMenu.SetActive(false);
-        controlSettingsMenu.SetActive(false);
-        languageMenu.SetActive(true);
-    }
-
     public void ShowPauseMenu() //Shows the pause menu
     {
         mainMenu.SetActive(false);
-        controlsMenu.SetActive(false);
         settingsMenu.SetActive(false);
         pauseMenu.SetActive(true);
         audioSettingsMenu.SetActive(false);
         controlSettingsMenu.SetActive(false);
-        languageMenu.SetActive(false);
         Time.timeScale = 0f; // Pauses the game by setting time to 0f
         Debug.Log("Game paused"); //Tells that the game is paused
     }
@@ -101,24 +66,20 @@ public class menuManager : MonoBehaviour
     public void ShowGameOverMenu()
     {
         mainMenu.SetActive(false);
-        controlsMenu.SetActive(false);
         settingsMenu.SetActive(false);
         pauseMenu.SetActive(false);
         audioSettingsMenu.SetActive(false);
         controlSettingsMenu.SetActive(false);
-        languageMenu.SetActive(false);
         gameOverMenu.SetActive(true);
     }
 
     public void ContinueGame() //Disables the pause menu
     {
         mainMenu.SetActive(false);
-        controlsMenu.SetActive(false);
         settingsMenu.SetActive(false);
         pauseMenu.SetActive(false);
         audioSettingsMenu.SetActive(false);
         controlSettingsMenu.SetActive(false);
-        languageMenu.SetActive(false);
         Time.timeScale = 1f; // Un-pauses the game by re-setting time to 1f
         Debug.Log("Game no longer paused"); //Tells that the game is no longer paused
     }
